d9745326e116bedfa219dfaa5a5ae451ffe5fed0
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-06-04 08:21:53 +0100

    Installed dotenv package and config for environment variables integration across all apps modules

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  // Load environment variables from .env file
+  dotenv.config();
+  
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     // .addBearerAuth()
     .setTitle('Fincra Customer Support Ticketing System')
-    .setDescription('Fincra Customer Support Ticketing System API Documentation')
+    .setDescription(
+      'Fincra Customer Support Ticketing System API Documentation',
+    )
     .setVersion('1.0')
     // .addTag('fincra, customer ticketing system')
     .build();
-  
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
