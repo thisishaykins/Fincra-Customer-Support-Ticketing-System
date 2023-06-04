880f46e3ac104ec1edfd59d8d174e33bf453000e
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-06-04 08:51:49 +0100

    Implementation included: JwtService, AuthService, UserService, UserSchema, and updated providers...

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtService } from '@nestjs/jwt';
 
 import { TicketController } from './ticket.controller';
 import { TicketService } from './ticket.service';
 import { TicketModel } from './ticket.model';
+import { AuthModule } from '../auth/auth.module';
+import { AuthService } from 'src/auth/auth.service';
+import { UserService } from 'src/user/user.service';
+import { UserSchema } from '../user/user.schema';
+
+
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Ticket', schema: TicketModel.schema }]),
+    MongooseModule.forFeature([
+      { name: 'Ticket', schema: TicketModel.schema },
+      { name: 'User', schema: UserSchema },
+    ]),
+    AuthModule,
   ],
   controllers: [TicketController],
-  providers: [TicketService],
+  providers: [TicketService, UserService, AuthService, JwtService],
 })
 export class TicketModule {}
