a46c9ff1145ff296e0e025a3c809b5fcd6bd0db9
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-06-04 10:12:07 +0100

    Added Mongoose module and mongodb connection string..

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +8,9 @@ import { TicketModule } from './ticket/ticket.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017/fincra-customer-support-ticketing-system',
+    ),
     TicketModule,
   ],
   controllers: [AppController],
