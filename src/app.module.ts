dc4f0da5d5b061e27840698ed58059c24fc0e2be
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-06-04 07:16:10 +0100

    Created the ticket module, service, controller, class and updated the app.module.ts...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TicketModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
